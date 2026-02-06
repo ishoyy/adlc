db3408e8d80f7b7e21b28c5ecd0c15d8f5c1ccb2
Isha A <ishoyy.a@gmail.com>
2026-02-06 03:01:14 -0500

    remove more folders

--- adlc/lib/firebase/client.ts
+++
@@ -1,6 +0,0 @@
-// Public config - safe in repository
-export const firebaseConfig = {
-  apiKey: "AIza...",  // Public, safe to commit
-  authDomain: "your-app.firebaseapp.com",
-  projectId: "your-project-id",
-};
